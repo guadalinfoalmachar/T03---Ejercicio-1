81ac8c75fd232795f4a5663a757d88c1b9580a1e
Juan Manuel España <guadalinfo.almachar@guadalinfo.es>
2022-06-28 17:39:52 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace T03_Eje1
+{
+    class T03_Eje1
+    {
+        static void Main()
+        {
+            int Valor1 = 580;
+            int Valor2 = 7;
+
+            Console.WriteLine("{0} + {1} = {2}", Valor1, Valor2, Valor1 + Valor2);
+            Console.WriteLine("{0} - {1} = {2}", Valor1, Valor2, Valor1 - Valor2);
+            Console.WriteLine("{0} * {1} = {2}", Valor1, Valor2, Valor1 * Valor2);
+            Console.WriteLine("{0} / {1} = {2}", Valor1, Valor2, Valor1 / Valor2);
+            Console.WriteLine("Resto de {0} / {1} = {2}", Valor1, Valor2, Valor1 % Valor2);
+
+            Console.WriteLine("Presiene cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
